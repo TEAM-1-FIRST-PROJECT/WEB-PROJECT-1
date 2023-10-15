9501bf18179ad837a200ace475a429a2346d09e8
TodorT <ttodorov95@icloud.com>
2023-10-15 22:41:59 +0300

    added styles for trending gif's

--- assets/src/views/trending-view.js
+++ assets/src/views/trending-view.js
@@ -3,10 +3,12 @@ import { renderFavoriteStatus } from "../events/favorite-events.js";
 export const toTrendingView = (trending) => {
     const gifs = trending.data.map((gif) => `
     
-    <div class="trending-gif">
+    <div class="image-container">
       <img src="${gif.images.fixed_height.url}">
-      <button class="view-trending-details-btn" data-trending-details-id="${gif.id}">Gif Details</button>
+      <div class="button-container">
+      <a href="#" class="view-trending-details-btn" data-trending-details-id="${gif.id}">Gif Details</a>
       ${renderFavoriteStatus(gif.id)}
+      </div>
     </div>
     
   `).join('');
