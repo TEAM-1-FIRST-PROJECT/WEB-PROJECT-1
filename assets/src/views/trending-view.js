be2d5b0ebb532574a18902de395efe7fc26e834f
Plamen Milanov <plamenpk@gmail.com>
2023-10-15 14:05:55 +0200

    add CSS

--- assets/src/views/trending-view.js
+++ assets/src/views/trending-view.js
@@ -1,14 +1,16 @@
 export const toTrendingView = (trending) => {
     const gifs = trending.data.map((gif) => `
+    
     <div class="trending-gif">
       <img src="${gif.images.fixed_height.url}">
       <button class="view-trending-details-btn" data-trending-details-id="${gif.id}">Gif Details</button>
     </div>
+    
   `).join('');
   
   return `
     <div id="trending">
-      <h1>Trending GIFs</h1>
+      <h1>Trending GIFs</h1>     
       <div class="trending-gifs-container">
         ${gifs}
       </div>
