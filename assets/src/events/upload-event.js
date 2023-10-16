d2ae47c6fa3245c055c65a2a0336b08ef18896e1
Plamen Milanov <plamenpk@gmail.com>
2023-10-16 16:13:43 +0200

    update upload-event

--- assets/src/events/upload-event.js
+++ assets/src/events/upload-event.js
@@ -1,24 +1,27 @@
-import { addUploadedGifs } from "../data/uploadedGifs.js";
-import { UPLOAD_URL, API_KEY } from "../common/constants.js";
+import { addUploadedGifs } from '../data/uploadedGifs.js';
+import { UPLOAD_URL, API_KEY } from '../common/constants.js';
 
 /**
  * Handles the submission of the upload form to upload a GIF.
- * @returns {void}
+ * @return {void}
  */
-export const uploadGif = () => { 
-  document.getElementById('uploadForm').addEventListener('submit', async (event)=>{
-    
+export const uploadGif = () => {
+  document.getElementById('uploadForm').addEventListener('submit', async (event) => {
+
     event.preventDefault();
     const formData = new FormData();
     const fileInput = document.getElementById('inputGif');
+    const username = document.getElementById('inputGif-username').value;
+    const tags = document.getElementById('inputGif-tags').value.trim();
     formData.append('file', fileInput.files[0]);
-    const url = UPLOAD_URL + API_KEY;
-  
+    //const url = `${UPLOAD_URL}${API_KEY}&username=${username}&tags=${tags}`;
+    const url = `${UPLOAD_URL}${API_KEY}&username=${username}&tags=${tags}`;
+    //const url = UPLOAD_URL + API_KEY;
     try {
       const response = await fetch(url, {
         method: 'POST',
         body: formData,
-      });      
+      });
       if (response.ok) {
         const data = await response.json();
         document.getElementById('uploadForm').innerHTML = `GIF uploaded successfully. ID: ${data.data.id}`;
--- assets/src/requests/request-service.js
+++ assets/src/requests/request-service.js
@@ -1,91 +1,93 @@
-import { BASE_URL } from "../common/constants.js"
-import { API_KEY } from "../common/constants.js"
-import { uploadGif } from "../events/upload-event.js"
+/* eslint-disable consistent-return */
+import { BASE_URL } from '../common/constants.js';
+import { API_KEY } from '../common/constants.js';
+import { uploadGif } from '../events/upload-event.js';
 
 /**
  * Uploads a new GIF.
- * @returns {function} .
+ * @return {function} .
   */
 export const uploadNewGif = () => {
-    return uploadGif();
-}
+  return uploadGif();
+};
 
 /**
  * Loads a collection of GIFs.
- * @returns {Promise<Object>} A promise that resolves with the GIFs data.
+ * @return {Promise<Object>} A promise that resolves with the GIFs data.
  * @throws {Error} If there is an error while fetching the GIFs.
  */
 export const loadHomePage = async () => {
-    const today = new Date();
-    const dayOfWeek = today.getDay();
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const dayName = daysOfWeek[dayOfWeek];
-    const url = `${BASE_URL}/search?q=${dayName}&api_key=${API_KEY}&limit=30` //`${BASE_URL}/${id}?api_key=${API_KEY}&rating=g`
-    try {
-        return (await fetch(url)).json();
-    } catch (error) {
-        console.error(error.message)
-    }
-}
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const dayName = daysOfWeek[dayOfWeek];
+  const url = `${BASE_URL}/search?q=${dayName}&api_key=${API_KEY}&limit=30`; // `${BASE_URL}/${id}?api_key=${API_KEY}&rating=g`
+  try {
+    return (await fetch(url)).json();
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 /**
  * Loads a single GIF by its ID.
  * @param {string} id - The ID of the GIF.
- * @returns {Promise<Object>} A promise that resolves with the GIF data.
+ * @return {Promise<Object>} A promise that resolves with the GIF data.
  * @throws {Error} If there is an error while fetching the GIF.
  */
+// eslint-disable-next-line consistent-return
 export const loadSingleGif = async (id) => {
-    const url = `${BASE_URL}/${id}?api_key=${API_KEY}&rating=g`
-    try {
-        return (await fetch(url)).json();
-    } catch (error) {
-        console.error(error.message)
-    }
-}
+  const url = `${BASE_URL}/${id}?api_key=${API_KEY}&rating=g`;
+  try {
+    return (await fetch(url)).json();
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 /**
  * Asynchronously loads a list of trending GIFs from the Giphy API.
  * @async
  * @function
- * @returns {Promise} A promise that resolves with the trending GIFs data.
+ * @return {Promise} A promise that resolves with the trending GIFs data.
  * @throws {Error} Throws if there is an error while fetching or parsing the response.
  */
 export const loadTrendingGif = async () => {
 
-    try {
-        const loadTrendingGifURL = `${BASE_URL}/trending?api_key=${API_KEY}`;
-        const response = await fetch(loadTrendingGifURL)
-        return response.json();
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    const loadTrendingGifURL = `${BASE_URL}/trending?api_key=${API_KEY}`;
+    const response = await fetch(loadTrendingGifURL);
+    return response.json();
+  } catch (error) {
+    console.log(error.message);
+  }
 
-}
+};
 
 export const loadGifDetails = async (id) => {
-    try {
-        const loadGifDetailsURL = `${BASE_URL}/${id}?api_key=${API_KEY}`;
-        const response = await fetch(loadGifDetailsURL);
-        return response.json();
-    } catch (error) {
-        console.error(error);
-    }
-}
+  try {
+    const loadGifDetailsURL = `${BASE_URL}/${id}?api_key=${API_KEY}`;
+    const response = await fetch(loadGifDetailsURL);
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 /**
  * Asynchronously loads a list of GIFs from the Giphy API based on a search query.
  * @async
  * @function
  * @param {string} searchTerm The search query used to find GIFs.
- * @returns {Promise} A promise that resolves with the GIFs data matching the search query.
+ * @return {Promise} A promise that resolves with the GIFs data matching the search query.
  * @throws {Error} Throws if there is an error while fetching or parsing the response.
  */
 export const loadSearchGif = async (searchTerm) => {
-    try {
-        const loadSearchGifURL = `${BASE_URL}/search?q=${searchTerm}&api_key=${API_KEY}&limit=12`;
-        const response = await fetch(loadSearchGifURL);
-        return response.json();
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const loadSearchGifURL = `${BASE_URL}/search?q=${searchTerm}&api_key=${API_KEY}&limit=12`;
+    const response = await fetch(loadSearchGifURL);
+    return response.json();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
