e8e05c3c42cb19b94aac6d3d17183adb67289397
lyubaintech <lyubaboyadzhieva@gmail.com>
2023-10-16 20:22:06 +0300

    about page css update

--- assets/src/views/about-view.js
+++ assets/src/views/about-view.js
@@ -4,11 +4,6 @@ export const toAboutView = () => `
     <h1>About the app</h1>
     <h2>Authors: Team 1</h2>
     <h2>Date: 16 October 2023</h2>
-    <ul> 
-      <li>Todor Todorov</li>
-      <li>Lyuba Boyadzhieva</li>
-      <li>Plamen Milanov</li>
-    </ul>
   </div>
 </div>
 `;
