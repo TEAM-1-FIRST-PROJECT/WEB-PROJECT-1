5de1cdcf4687db8dbebc1526ded1cc6443c37fbe
Plamen Milanov <plamenpk@gmail.com>
2023-10-17 12:04:27 +0200

    add hover uploaded-gifs-view

--- assets/src/views/uploaded-gifs-view.js
+++ assets/src/views/uploaded-gifs-view.js
@@ -4,20 +4,22 @@ import { renderFavoriteStatus } from '../events/favorite-events.js';
  * @return {void}
  */
 export const toUploadedGifsView = (uploaded) => {
-
   const gifs = uploaded.map((gif) => `
   
-    <div class="uploaded-gif">
-      <img src="${gif.data.images.fixed_height.url}">
+  <div class="image-container">
+    <img src="${gif.data.images.fixed_height.url}">
+      <div class="button-container">
       <button class="view-trending-details-btn" data-trending-details-id="${gif.data.id}">View Details</button>
       <button class="favorites-button">${renderFavoriteStatus(gif.data.id)}</button>
-    </div>  
-  `).join('');
-  return `
-      <div id="uploaded">
-        <div class="uploaded-gifs-container">
-          ${gifs || '<p class="upload-form-message">Upload some GIF to see them here.</p>'} 
-        </div>
-      </div>
-    `;
+    </div>
+  </div>  
+`).join('');
+
+return `
+  <div id="trending">   
+    <div class="trending-gifs-container">
+      ${gifs|| '<p class="upload-form-message">Upload some GIF to see them here.</p>'}
+    </div>
+  </div>
+  `;
 };
