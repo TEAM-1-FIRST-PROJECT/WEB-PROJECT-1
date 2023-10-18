8594653125ea94ea871c74c3e642b39e5dbaf827
TodorT <ttodorov95@icloud.com>
2023-10-18 12:19:32 +0300

    added JS Doc

--- assets/src/data/favorites.js
+++ assets/src/data/favorites.js
@@ -1,5 +1,11 @@
 let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
+/**
+ * Adds a GIF ID to the list of favorites.
+ * 
+ * @param {string} gifId - The ID of the GIF to be added to the favorites list.
+ * @returns {void}
+ */
 export const addFavorite = (gifId) => {
   if (favorites.find(id => id === gifId)) {
     // Movie has already been added to favorites
@@ -10,9 +16,27 @@ export const addFavorite = (gifId) => {
   localStorage.setItem('favorites', JSON.stringify(favorites));
 };
 
+
+/**
+ * Removes a specified GIF ID from the favorites array and updates the localStorage with the updated favorites array.
+ * 
+ * @param {string} gifId - The ID of the GIF to be removed from the favorites array.
+ * @returns {void} - This function does not return anything.
+ */
 export const removeFavorite = (gifId) => {
+
   favorites = favorites.filter(id => id !== gifId);
   localStorage.setItem('favorites', JSON.stringify(favorites));
 };
 
-export const getFavorites = () => [...favorites];
+/**
+   * Returns a copy of the favorites array retrieved from the local storage.
+   * If the favorites array is not found in the local storage, it defaults to an empty array.
+   * 
+   * @returns {Array} A new array that contains the same elements as the favorites array.
+   * 
+   * @example
+   * const favorites = getFavorites();
+   * console.log(favorites); // [item1, item2, item3]
+   */
+export const getFavorites = () => { return [...favorites]; };
--- assets/src/events/search-events.js
+++ assets/src/events/search-events.js
@@ -2,6 +2,12 @@ import { loadSearchGif } from '../requests/request-service.js';
 import { CONTAINER_SELECTOR } from '../common/constants.js';
 import { toSearchView } from '../views/search-view.js';
 import { q } from './helpers.js';
+
+/**
+  * Fetches GIFs related to the search term and updates the HTML content of a container element with the search results.
+  * @param {string} searchTerm - The term to search for GIFs.
+  * @returns {void}
+  */
 export const renderSearchItems = (searchTerm) => {
   loadSearchGif(searchTerm)
     .then(gifs => q(CONTAINER_SELECTOR).innerHTML = toSearchView(gifs.data, searchTerm));
